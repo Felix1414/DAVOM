4c6cffda747ea5d07b5774db34863752def2ad13
Felix1414 <fabian14.gonzalez.fg@gmail.com>
2024-07-25 22:24:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 app = Flask(__name__)
 
 # Configura tu clave API
-openai.api_key = 'sk-proj-7JQ0XqwiR3X2S726OkvQT3BlbkFJSJzSklT1XDwtBDZtsFlo'
+openai.api_key = 'sk-proj-wS3T68PWfRLvvUlIzeklT3BlbkFJ0ix5VhaYkPU2ZI8qmWCS'
 
 @app.route('/')
 def index():
